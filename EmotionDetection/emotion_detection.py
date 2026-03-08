84efb32055ecb6f3f4f26c2db4505d1fbab7bfd4
Mino990 <srahaman2690@gmail.com>
2026-03-08 03:39:37 -0400

    Added emotion detection package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    """Analyze the emotion of the input text"""
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=payload, headers=headers)
+    formatted_response = json.loads(response.text)
+    emotion_json = formatted_response["emotionPredictions"][0]["emotion"]
+    
+    if response.status_code == 200:
+        emotion_json["dominant_emotion"] = max(emotion_json, key=emotion_json.get)
+        emotion = emotion_json
+    elif response.status_code == 500:
+        emotion = None
+
+    return emotion 
